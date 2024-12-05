a3873b1eb8f6d7df78358f7376d46132c917224b
acepect0 <rubencardosogaspar@gmail.com>
2024-12-05 19:34:25 +0000

    feat: adicionada função de adição

--- run.py
+++ run.py
@@ -7,3 +7,15 @@ def main():
     """
     Essa é a função principal.
     """
+
+def add():
+    """
+    Função da operação de adição.
+    """
+    
+    numOne = int(input("Type the first number: "))
+    numTwo = int(input("Type the second number: "))
+
+    result = numOne + numTwo
+
+    print(f"The result is {result}")
